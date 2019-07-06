06e710d1df4c798d2f7ee1f3a13f906867130795
William L <w-lindvall@outlook.com>
2019-07-06 16:33:38 -0700

    move site description to head

--- themes/tent/header.php
+++ themes/tent/header.php
@@ -12,7 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>"/>
 	<?php wp_head(); ?>
 	</head>
 
@@ -23,7 +23,6 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
