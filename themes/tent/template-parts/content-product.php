bae735a89dea0b31f209115ad894952cbf526398
William L <w-lindvall@outlook.com>
2019-07-12 23:19:11 -0700

    remove `bookmark` rel attr

--- themes/tent/template-parts/content-product.php
+++ themes/tent/template-parts/content-product.php
@@ -16,7 +16,7 @@
     <?php
       the_title(
         sprintf(
-          '<h2 class="entry-title"><a href="%s" rel="bookmark">',
+          '<h2 class="entry-title"><a href="%s">',
           esc_url(get_permalink())
         ),
         '</a></h2>'
