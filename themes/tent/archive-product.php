cf6f2d9aca080c8e3900e5d783597b081e24a56e
William L <w-lindvall@outlook.com>
2019-07-13 14:38:16 -0700

    query recent posts

--- themes/tent/archive-product.php
+++ themes/tent/archive-product.php
@@ -10,36 +10,43 @@ get_header(); ?>
 	<div id="primary">
 		<main id="main" class="site__main" role="main">
 
-    <?php global $query_string;
-      $posts = query_posts($query_string.'&posts_per_page=16');
-    ?>
+      <?php
+        $product_posts = get_posts(array(
+          'post_type' => 'product',
+          'posts_per_page' => 16,
+        ));
+      ?>
 
-		<?php if (have_posts()) : ?>
+        <?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<?php
-					the_archive_title('<h2 class="page-title">', '</h2>');
-					the_archive_description('<div class="taxonomy-description">', '</div>');
-				?>
-			</header>
+          <header class="page-header">
+            <?php
+              the_archive_title('<h2 class="page-title">', '</h2>');
+              the_archive_description('<div class="taxonomy-description">', '</div>');
+            ?>
+          </header>
 
-      <section class="shop__grid">
+          <section class="shop__grid">
 
-        <?php while (have_posts()) : the_post(); ?>
-          <?php get_template_part( 'template-parts/content', 'product' ); ?>
-        <?php endwhile; ?>
+            <?php
+              foreach ( $product_posts as $post ) : setup_postdata( $post );
+            ?>
 
-      </section>
+              <?php get_template_part( 'template-parts/content', 'product' ); ?>
 
-			<?php the_posts_navigation(); ?>
+            <?php
+              endforeach; wp_reset_postdata();
+            ?>
 
-		<?php else : ?>
+          </section>
 
-			<?php get_template_part('template-parts/content', 'none'); ?>
+          <?php the_posts_navigation(); ?>
 
-		<?php endif; ?>
+        <?php else : ?>
 
-    <?php wp_reset_query(); ?>
+          <?php get_template_part('template-parts/content', 'none'); ?>
+
+        <?php endif; ?>
 
 		</main>
 	</div>
--- themes/tent/front-page.php
+++ themes/tent/front-page.php
@@ -14,8 +14,26 @@ get_header(); ?>
 				<?php get_template_part('template-parts/content', 'page'); ?>
 			<?php endwhile; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+      <h2>Inhabitent Journal</h2>
+      <div class="flex">
+        <?php
+          $blog_posts = get_posts(array(
+            'post_type' => 'post',
+            'posts_per_page' => 3,
+          ));
+
+          foreach ( $blog_posts as $post ) : setup_postdata( $post );
+        ?>
+
+          <?php get_template_part('template-parts/content', 'thumbnail'); ?>
+
+        <?php
+          endforeach; wp_reset_postdata();
+        ?>
+      </div>
+        
+		</main>
+	</div>
 
 <?php if (!has_block('tent/hero-image-header')) : get_sidebar(); endif; ?>
 <?php get_footer(); ?>
