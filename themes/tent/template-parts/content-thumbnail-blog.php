23af26812c5adc080581e5c5306a6065e95b56a7
William L <w-lindvall@outlook.com>
2019-07-18 12:52:02 -0700

    style front page

--- themes/tent/template-parts/content-thumbnail-blog.php
+++ themes/tent/template-parts/content-thumbnail-blog.php
@@ -1,41 +1,41 @@
 <?php
 /**
- * Posts thumbnail template 
+ * Journal posts thumbnail template 
  *
  * @package tent
  */
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry__header">
-		<?php if (has_post_thumbnail()) : ?>
+<div id="blog-thumbnail-<?php the_ID(); ?>" <?php post_class('blog-thumbnail'); ?>>
+  <div class="blog-thumbnail__img-wrapper">
+    <?php if (has_post_thumbnail()) : ?>
       <?php
         the_post_thumbnail(
           'large',
-          ['class' => 'entry__header-img']
+          ['class' => 'entry__header-img blog-thumbnail__img']
         );
       ?>
-		<?php endif; ?>
+    <?php endif; ?>
+  </div>
 
-    <h3 class="entry__title">
+	<header class="entry__header blog-thumbnail__header">
+    <div class="entry__meta blog-thumbnail__meta">
+      <?php tent_posted_on(); ?> /
+      <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?>
+    </div>
+
+    <h3 class="entry__title blog-thumbnail__title">
       <a class="entry__title__link" href="<?= esc_url(get_permalink()); ?>">
         <?= the_title(); ?>
       </a>
     </h3>
 
-    <div class="entry__meta">
-      <?php tent_posted_on(); ?> /
-      <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?>
-    </div>
-	</header>
-
-	<div class="entry__content">
-    <a class="button" href="<?= esc_url(get_permalink()); ?>">
+    <a class="button blog-thumbnail__action" href="<?= esc_url(get_permalink()); ?>">
       Read entry
       <span class="button__icon" role="img" aria-label="">
         &#8594;
       </span>
     </a>
-	</div>
-</article>
+	</header>
+</div>
