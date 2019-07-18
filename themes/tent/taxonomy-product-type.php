76465c20b5bb1d88e9b90067b80b94a58fc9dd74
William L <w-lindvall@outlook.com>
2019-07-17 17:16:31 -0700

    clean up product type term query

--- themes/tent/taxonomy-product-type.php
+++ themes/tent/taxonomy-product-type.php
@@ -11,41 +11,23 @@ get_header(); ?>
 		<main class="site__main">
 
       <header class="page__header">
-        <?php
-          $active_term = get_queried_object();
-        ?>
+
         <h2 class="page__title">
-          <?= $active_term->name; ?>
+          <?= single_term_title(); ?>
         </h2>
 
         <div class="shop__description">
-          <?= $active_term->description; ?>
+          <?= the_archive_description(); ?>
         </div>
 
       </header>
-
-      <?php
-        $product_posts = get_posts(array(
-          'post_type' => 'product',
-          'posts_per_page' => 4,
-          'tax_query' => array(
-            array(
-              'taxonomy' => 'product-type',
-              'field' => 'slug',
-              'terms' => $active_term->slug, 
-              'include_children' => true,
-              'operator' => 'IN'
-            )
-          ),
-        ));
-      ?>
 
         <?php if (have_posts()) : ?>
 
           <section class="shop__grid">
 
             <?php
-              foreach ( $product_posts as $post ) : setup_postdata( $post );
+              foreach ( $posts as $post ) : setup_postdata( $post );
             ?>
 
               <?php get_template_part( 'template-parts/content', 'product' ); ?>
