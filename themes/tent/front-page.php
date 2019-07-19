77f85472e4ecdf357a89b2e801fda668825717e5
William L <w-lindvall@outlook.com>
2019-07-19 13:23:47 -0700

    more adventures btn

--- themes/tent/front-page.php
+++ themes/tent/front-page.php
@@ -13,64 +13,77 @@ get_header(); ?>
       <?php get_template_part('template-parts/content', 'page'); ?>
     <?php endwhile; ?>
 
-    <h2 class="entry__title entry__title--light">Shop Stuff</h2>
-    <div class="flex">
-      <?php
-        $product_types_terms = get_terms(array(
-          'taxonomy' => 'product-type',
-          'count' => 4,
-        ));
+    <section class="page__section">
+      <h2 class="entry__title entry__title--light">Shop Stuff</h2>
+      <div class="flex">
+        <?php
+          $product_types_terms = get_terms(array(
+            'taxonomy' => 'product-type',
+            'count' => 4,
+          ));
 
-        foreach ($product_types_terms as $term) :
-          $term_name = $term->name;
-          $term_description = $term->description;
-          $term_url = get_term_link($term);
-          $term_icon_url =
-            get_template_directory_uri().
-            '/images/product-type__'.
-            $term_name.
-            '.svg';
-        
-          // load template part in this scope
-          include(locate_template('template-parts/content-thumbnail-shop.php', false, false));
-        
-        endforeach;
+          foreach ($product_types_terms as $term) :
+            $term_name = $term->name;
+            $term_description = $term->description;
+            $term_url = get_term_link($term);
+            $term_icon_url =
+              get_template_directory_uri().
+              '/images/product-type__'.
+              $term_name.
+              '.svg';
+          
+            // load template part in this scope
+            include(locate_template('template-parts/content-thumbnail-shop.php', false, false));
+          
+          endforeach;
 
-      ?>
-    </div>
+        ?>
+      </div>
+    </section>
 
-    <h2 class="entry__title entry__title--light">Inhabitent Journal</h2>
-    <div class="flex">
-      <?php
-        $blog_posts = get_posts(array(
-          'post_type' => 'post',
-          'posts_per_page' => 3,
-        ));
+    <section class="page__section">
+      <h2 class="entry__title entry__title--light">Inhabitent Journal</h2>
+      <div class="flex">
+        <?php
+          $blog_posts = get_posts(array(
+            'post_type' => 'post',
+            'posts_per_page' => 3,
+          ));
 
-        foreach ($blog_posts as $post) : setup_postdata($post);
+          foreach ($blog_posts as $post) : setup_postdata($post);
 
-          get_template_part('template-parts/content', 'thumbnail-blog');
+            get_template_part('template-parts/content', 'thumbnail-blog');
 
-        endforeach; wp_reset_postdata();
-      ?>
-    </div>
+          endforeach; wp_reset_postdata();
+        ?>
+      </div>
+    </section>
       
-    <h2 class="entry__title entry__title--light">Latest Adventures</h2>
-    <div class="adventures-thumbnail__wrapper">
-      <?php
-        $adventure_posts = get_posts(array(
-          'post_type' => 'adventure',
-          'posts_per_page' => 4,
-          'order' => 'ASC',
-        ));
+    <section class="page__section page__section--left">
+      <h2 class="entry__title entry__title--light">Latest Adventures</h2>
+      <div class="adventures-thumbnail__wrapper">
+        <?php
+          $adventure_posts = get_posts(array(
+            'post_type' => 'adventure',
+            'posts_per_page' => 4,
+            'order' => 'ASC',
+          ));
 
-        foreach ($adventure_posts as $post) : setup_postdata($post);
+          foreach ($adventure_posts as $post) : setup_postdata($post);
 
-          get_template_part('template-parts/content', 'thumbnail-adventures');
+            get_template_part('template-parts/content', 'thumbnail-adventures');
 
-        endforeach; wp_reset_postdata();
-      ?>
-    </div>
+          endforeach; wp_reset_postdata();
+        ?>
+      </div>
+
+      <a
+        class="button button--secondary adventures-thumbnail__action"
+        href="<?= get_post_type_archive_link('adventure'); ?>"
+      >
+        More Adventures
+      </a>
+    </section>
   </main>
 
 <?php if (!has_block('tent/hero-image-header')) : get_sidebar(); endif; ?>
